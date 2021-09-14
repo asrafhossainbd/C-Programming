ba3b343d2b50fd2d4b007567a0ff99f4d154b655
IT-Asraf <itasrafhossainbd@gmail.com>
2021-09-15 01:22:06 +0600

    Experimetn: How ot knows Type Cast value is not change

--- Day_01/start.c
+++ Day_01/start.c
@@ -52,7 +52,7 @@ int main()
 
     //Experiment:
     //Using Real Number(Ex. 40.35) in integer Data type
-
+    /*
     int a = 50.45, b = 60, sum;
 
     sum = a + b;
@@ -61,7 +61,19 @@ int main()
     
     //Ans: 50 + 60 = 110 
     //Note: This is Call (Type Cast)
-    
+    */
+
+    //Experiment:
+    //How to knows Type Cast Value is not Change
+
+    int n;
+    double x; //double" data type, it career Real Number value
+
+    x = 10.5;
+    n = (int)x;
+
+    printf("Value of n is %d\n", n); //Ans.: Value of n is 10 Note: type cust hare, x was double data type than x convart in int data type 
+    printf("Value of x is %lf\n", x); //Ans.: Value of x is 10.500000
 
     return 0;
 }
