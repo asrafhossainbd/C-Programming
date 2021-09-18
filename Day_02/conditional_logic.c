4d0ee256faa815e36ad30a07b7c79e55b7e6a93a
IT-Asraf <itasrafhossainbd@gmail.com>
2021-09-19 01:20:46 +0600

    Practice if & else

---
+++ Day_02/conditional_logic.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+
+    n = 10;
+
+    /*
+    if(n >= 0){
+        printf("The number is positive\n");
+    }
+    else{
+        printf("The number is negative\n");
+    }
+    */
+
+    if(n < 0){
+        printf("The number is negetive\n");
+    }
+    else{
+        printf("The number is positive\n");
+    }
+
+    return 0;
+}
