a107f09f15afac6552102583e4ffc272fdf5388e
IT-Asraf <itasrafhossainbd@gmail.com>
2021-09-20 00:26:03 +0600

    Using if, else if & else

--- Day_02/conditional_logic.c
+++ Day_02/conditional_logic.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main()
 {
+    /*
     int n;
 
     n = 10;
 
-    /*
     if(n >= 0){
         printf("The number is positive\n");
     }
@@ -13,6 +13,10 @@ int main()
         printf("The number is negative\n");
     }
     */
+    /*
+    int n;
+
+    n = 10;
 
     if(n < 0){
         printf("The number is negetive\n");
@@ -20,6 +24,20 @@ int main()
     else{
         printf("The number is positive\n");
     }
+    */
 
+    int n = 10;
+
+    if(n < 0){
+        printf("The number is negative\n");
+    }
+    else if (n > 0)
+    {
+        printf("The number is positive\n");
+    }
+    else{
+        printf("The number is zero\n");
+    }
+    
     return 0;
 }
