1d97e3bc839b08b392765e2135678e4c41293f25
IT-Asraf <itasrafhossainbd@gmail.com>
2021-09-20 01:35:09 +0600

    Basic problem solve, How to find out even or odd number?

--- Day_02/conditional_logic.c
+++ Day_02/conditional_logic.c
@@ -41,7 +41,7 @@ int main()
     */
 
     //Experiment 
-
+    /*
     int n = 10;
     if(n < 30){
         printf("n is less the 30.\n");
@@ -49,6 +49,21 @@ int main()
     else if(n < 50){
         printf("n is less the 50.\n");
     }
-    
+    */
+
+    // How to find out even or odd number
+
+    int number;
+
+    printf("Enter a number: ");
+
+    scanf("%d", &number);
+
+    if(number % 2 <= 0 ){
+        printf("%d is even number.", number);
+    }else{
+        printf("%d is odd number.", number);
+    }
+
     return 0;
 }
